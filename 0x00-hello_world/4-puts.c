2ac3a1a64c59f07b480ab79ea04941dd3d6e758f
Redemta <redzawy@gmail.com>
2023-08-10 14:14:37 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+/**
+ * main - using puts function to print a line.
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
