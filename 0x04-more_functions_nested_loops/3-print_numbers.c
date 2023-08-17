ea0496185f5072aa66f4d96d183d5607255b0278
Redemta <redzawy@gmail.com>
2023-08-17 12:06:40 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - print numbers from 0 to 9.
+ *
+ * Return: Always 0 (success)
+ */
+
+void print_numbers(void)
+{
+	int x;
+
+	for (x = 48; x < 58; x++)
+	{
+		_putchar(x);
+	}
+		_putchar('\n');
+}
