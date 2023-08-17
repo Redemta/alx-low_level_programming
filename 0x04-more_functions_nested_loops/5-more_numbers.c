15f4c87a56e7aaf5a48bf51a29c24be3cf404344
Redemta <redzawy@gmail.com>
2023-08-17 13:18:26 -0700

    5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * more_numbers - print 10 times numbers from 0 to 14
+ *
+ * Return: Always (0)
+ */
+
+void more_numbers(void)
+{
+	int x;
+	int y;
+
+	for (x = 0; x < 10; x++)
+	{
+		for (y = 0; y < 15; y++)
+		{
+			if (y >= 10)
+			_putchar(y / 10 + '0');
+			_putchar(y % 10 + '0');
+			
+		}
+		_putchar('\n');
+	}
+		
+}
