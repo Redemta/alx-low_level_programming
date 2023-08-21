1f48673147c3dd8536f7e23aff7fd964d3dacf50
Redemta <redzawy@gmail.com>
2023-08-21 13:31:50 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts - function that prints a string, followed by a new line
+ * @str: char to check
+ * Return: Always 0
+ */
+
+void _puts(char *str)
+{
+	int x;
+
+	for (x = 0; str[x] != '\0'; x++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
+}
