d9a854919222837c12f998f2eeff873b80c6ca96
Redemta <redzawy@gmail.com>
2023-08-31 13:35:14 -0700

    101-wildcmp.c

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -25,7 +25,7 @@ int wildcmp(char *s1, char *s2)
 	{
 		return (wildcmp(s1 + 1, s2 + 1));
 	}
-	if (*s2 == '*')
+	if (*s2 == '*' && *(s2 + 1) != '\0')
 	{
 		int option1 = wildcmp(s1 + 1, s2);
 		int option2 = wildcmp(s1, s2 + 1);
