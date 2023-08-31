d406ab0c7205aec5f86653ceb0e4f1ce02987e30
Redemta <redzawy@gmail.com>
2023-08-31 13:25:36 -0700

    101-wildcmp.c

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,48 +1,40 @@
 #include "main.h"
 
 /**
- * wildcard - helper function.
+ * wildcmp -function that compares two strings.
  * @s1: string one.
  * @s2: string two.
  * Return: 0.
  */
 
-int wildcard(char *s1, char *s2)
+int wildcmp(char *s1, char *s2)
 {
 	if (*s1 == '\0' && *s2 == '\0')
 	{
 		return (1);
 	}
-	if (*s1 == '\0' || *s2 == '\0')
+	if (*s1 == '\0' && *s2 == '*')
+	{
+		return (wildcmp(s1, s2  + 1));
+	}
+	if (*s1 != '\0' && *s2 == '\0')
 	{
 		return (0);
 	}
+	if (*s1 == *s2 || *s2 == '*')
+	{
+		return (wildcmp(s1 + 1, s2 + 1));
+	}
 	if (*s2 == '*')
 	{
-		if (*(s2 + 1) == '*')
-		{
-			return (wildcard(s1, s2 + 1));
-		}
-		if (wildcard(s1, s2 + 1) || wildcard(s1 + 1, s2))
+		int option1 = wildcmp(s1 + 1, s2);
+		int option2 = wildcmp(s1, s2 + 1);
+
+		if (option1 || option2)
 		{
 			return (1);
 		}
 	}
-	if (*s1 != *s2)
-	{
-		return (0);
-	}
-	return (wildcard(s1 + 1, s2 + 1));
+	return (0);
 }
 
-/**
- * wildcmp - function that compares two strings.
- * @s1: string one.
- * @s2: string two.
- * Return: 1 if identical, 0 if otherwise.
- */
-
-int wildcmp(char *s1, char *s2)
-{
-	return (wildcard(s1, s2));
-}
