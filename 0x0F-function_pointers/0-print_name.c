e0b40944ec28dba0af975662c6b72a18315e2da7
Redemta <redzawy@gmail.com>
2023-09-13 02:15:08 -0700

    0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -4,20 +4,13 @@
  * print_name - function that prints a name.
  * @name: pointer to string that represents name to print.
  * @f: pointer to function,takes char * as parameter, and returns void.
- * Return: Always 0.
  */
 
 void print_name(char *name, void (*f)(char *))
 {
+	if (name == NULL || f == NULL)
+	{
+		return;
+	}
 	f(name);
-}
-
-/**
- * custom_printer - f parameter to print_name.
- * @str: string parameter.
- */
-
-void custom_printer(char *str)
-{
-	printf("Custom Printer: %s\n", str);
 }
