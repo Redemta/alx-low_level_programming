0219b9d8286b446d9c4200b04e4f914b4e57534c
Redemta <redzawy@gmail.com>
2023-09-21 14:29:39 -0700

    100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void custom_message(void);
+
+/**
+ * custom_message - function to print message before executing main function.
+ */
+
+void custom_message(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
